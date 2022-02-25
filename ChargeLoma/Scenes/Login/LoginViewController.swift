337214517443ca58d4b75559d258e7cf23a32a79
nontawatkb <nontawatjss@gmail.com>
2022-02-25 23:36:04 +0700

    add method apple login

--- ChargeLoma/Scenes/Login/LoginViewController.swift
+++ ChargeLoma/Scenes/Login/LoginViewController.swift
@@ -11,6 +11,8 @@ import GoogleSignIn
 import FBSDKCoreKit
 import FBSDKLoginKit
 
+import AuthenticationServices
+
 public protocol LoginDelegate {
     func didLoginSuccess(actionType: LoginActionType)
 }
@@ -34,6 +36,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var btnFacebook: UIButton!
     @IBOutlet weak var btnGoogle: UIButton!
 
+    @IBOutlet weak var btnApple: UIButton!
     public var delegate: LoginDelegate? = nil
     
     public var actionType: LoginActionType = .unknown
@@ -103,6 +106,7 @@ class LoginViewController: UIViewController {
         self.btnFacebook.addTarget(self, action: #selector(handleFacebookSignin), for: .touchUpInside)
         self.btnGoogle.addTarget(self, action: #selector(handleGoogleSignin), for: .touchUpInside)
         self.btnRegister.addTarget(self, action: #selector(handleSignup), for: .touchUpInside)
+        self.btnApple.addTarget(self, action: #selector(handleApple), for: .touchUpInside)
     }
 
 }
@@ -159,6 +163,10 @@ extension LoginViewController {
     
     @objc func handleSignup() {
         NavigationManager.instance.pushVC(to: .editProfile(isRegister: true, delegate: self), presentation: .Present(withNav: false))
+    }
+    
+    @objc func handleApple() {
+        performSignInAppleID()
     }
 }
 
@@ -242,4 +250,110 @@ extension LoginViewController: EditProfileViewControllerDelegate {
             self.delegate?.didLoginSuccess(actionType: self.actionType)
         })
     }
+}
+
+
+//MARK: - Apple Auth
+extension LoginViewController : ASAuthorizationControllerPresentationContextProviding, ASAuthorizationControllerDelegate{
+    func performSignInAppleID() {
+        let request = createAppleIDRequest()
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    func createAppleIDRequest() -> ASAuthorizationAppleIDRequest {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let nonce = randomNonceString()
+        request.nonce = sha256(nonce)
+        currentNonce = nonce
+        
+        return request
+    }
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCreadential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else {
+                fatalError("Invalid state: A login callback was received. but no login request was sent")
+            }
+            guard let appleIDToken = appleIDCreadential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to to serialize token string from date: \(appleIDToken.debugDescription)")
+                return
+            }
+            let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
+            self.startLoding()
+            
+            Auth.auth().signIn(with: credential, completion: { (user, error) in
+                self.startLoding()
+                if let error = error {
+                    print("Login Error : \(error.localizedDescription)")
+                    self.stopLoding()
+                }else {
+                    guard let user = user else {
+                        return
+                    }
+                    self.checkNewAuth(user: user)
+                }
+            })
+        }
+    }
+}
+
+private func randomNonceString(length: Int = 32) -> String {
+    precondition(length > 0)
+    let charset: Array<Character> =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+    var result = ""
+    var remainingLength = length
+    
+    while remainingLength > 0 {
+        let randoms: [UInt8] = (0 ..< 16).map { _ in
+            var random: UInt8 = 0
+            let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+            if errorCode != errSecSuccess {
+                fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+            }
+            return random
+        }
+        
+        randoms.forEach { random in
+            if remainingLength == 0 {
+                return
+            }
+            
+            if random < charset.count {
+                result.append(charset[Int(random)])
+                remainingLength -= 1
+            }
+        }
+    }
+    
+    return result
+}
+
+import CryptoKit
+
+fileprivate var currentNonce: String?
+
+@available(iOS 13, *)
+private func sha256(_ input: String) -> String {
+    let inputData = Data(input.utf8)
+    let hashedData = SHA256.hash(data: inputData)
+    let hashString = hashedData.compactMap {
+        return String(format: "%02x", $0)
+    }.joined()
+    
+    return hashString
 }
